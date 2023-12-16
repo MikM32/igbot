0b6d70d52c3f01aec3e5eb1b5b1836803772ef23
MikM32 <miguelmatute2004@gmail.com>
2023-12-16 00:48:10 -0400

    implementacion del metodo IgBot.login() y metodo IgBot.init_ig_main()

--- igbot.py
+++ igbot.py
@@ -4,7 +4,9 @@ from selenium.webdriver.chrome.options import Options as COptions
 from selenium.webdriver.firefox.options import Options as FOptions
 from selenium.webdriver.chrome.service import Service as CService
 from selenium.webdriver.firefox.service import Service as FService
+from selenium.webdriver.common.by import By
 import default_browser
+import time, random
 from constants import *
 
 def get_webdriver_path(browser_type: str) -> str:
@@ -21,10 +23,10 @@ def get_webdriver_path(browser_type: str) -> str:
 
 class IgBot:
 
-    def __init__(self, default_browser: bool=True, *args):
+    def __init__(self, is_def_browser: bool=True, *args: str):
         
         self.def_browser = default_browser.get_browser_exepath()
-        if(default_browser):
+        if(is_def_browser):
             self.webdriver_path = get_webdriver_path(self.def_browser)
         else:
             self.webdriver_path = args[0]
@@ -32,20 +34,47 @@ class IgBot:
         self.browser_handler = None
         self.options = None
         self.service = None
+        self.sleep_secs = None
 
         if "Chrome" in self.def_browser:
             self.options = COptions()
-            self.options.binary_location = self.webdriver_path
-            self.service = CService(executable_path=self.def_browser)
+            #self.options.add_argument("--user-data-dir={}".format(CHROME_USER_DATA))
+            self.options.add_argument('--profile-directory=Default')
+            self.options.binary_location = self.def_browser
+            self.service = CService(executable_path=self.webdriver_path)
             self.browser_handler = webdriver.Chrome(options=self.options, service=self.service)
         elif "Firefox" in self.def_browser:
             self.options = FOptions()
-            self.options.binary = self.webdriver_path
-            self.service = FService(executable_path=self.def_browser)
+            self.options.binary = self.def_browser
+            self.service = FService(executable_path=self.webdriver_path)
             self.browser_handler = webdriver.Firefox(options=self.options, service=self.service)
         else:
             raise Exception("Navegador predeterminado no soportado")
-        
+    
+    def init_ig_main(self):
+        self.browser_handler.implicitly_wait(5)
+        self.browser_handler.get(IG_URL)
+
+    def login(self, user:str, pwd:str):
+        self.wait()
+        username_input = self.browser_handler.find_element(By.XPATH, USER_INPUT_XPATH)
+        pwd_input = self.browser_handler.find_element(By.XPATH, PWD_INPUT_XPATH)
+
+        username_input.send_keys(user)
+        pwd_input.send_keys(pwd)
+
+        login_button = self.browser_handler.find_element(By.XPATH, LOGIN_BUTTON_XPATH)
+        login_button.click()
+
+        self.wait()
+    
+    def wait(self):
+        self._update_sleep_secs()
+        time.sleep(self.sleep_secs)
+
+    def _update_sleep_secs(self):
+        self.sleep_secs = random.randint(5, 12)
+
     def close_browser_handler(self):
         self.browser_handler.quit()
 
@@ -53,6 +82,10 @@ class IgBot:
 
 def main():
     bot = IgBot()
+    bot.init_ig_main()
+    #bot.login()
+    time.sleep(1000)
+    bot.close_browser_handler()
 
 if __name__ == "__main__":
     main()
